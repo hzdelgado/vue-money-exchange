27598372c45700ff235fa99902c82e3e81ee878c
hzdelgado <88523514+hzdelgado@users.noreply.github.com>
2023-11-25 15:43:57 -0500

    componentes removidos
    tailwind instalado

---
+++ src/views/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type ExchangeBoxVue from '@/components/ExchangeBox.vue'
+</script>
+
+<template>
+  <div class="bg-indigo-500 w-screen h-screen">
+    <header>
+      <TheWelcome />
+    </header>
+
+    <main>
+      <ExchangeBoxVue />
+    </main>
+  </div>
+</template>
